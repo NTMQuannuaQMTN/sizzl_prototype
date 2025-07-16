20f8f3ad7cf40e4882da9a6672dabe64f2df0bc3
duy <courtinsss13@gmail.com>
2025-07-16 09:12:28 +0700

    small change

--- app/(create)/create.tsx
+++ app/(create)/create.tsx
@@ -622,7 +622,7 @@ export default function CreatePage() {
         </View>
 
         {/* More settings modal */}
-        <View style={tw`px-4 mb-20`}>
+        <View style={tw`px-4 mb-16`}>
           <TouchableOpacity
             style={tw`flex-row items-center gap-2.5 bg-white/10 rounded-xl px-3.5 py-2 mt-2`}
             onPress={() => setShowMoreSettingsModal(true)}
